2c4b09167a5197c52039025c29d7b6ffc86287f9
pashtet <pashtetbezd@gmail.com>
2018-07-20 17:55:04 +0500

    analitics

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,7 @@ module.exports = {
   ],
   plugins: [
     // ssr: false to only include it on client-side
-    //{ src: '~/plugins/ga.js', ssr: false },
+    { src: '~/plugins/ga.js', ssr: false },
     //{ src: '~/plugins/vue-socket.js', ssr: false },
     { src: '~/plugins/social.js' },
     { src: '~/plugins/vue-observe-visibility.js', ssr: false  },
@@ -134,15 +134,15 @@ module.exports = {
   },
 
   modules: [
-    // ['@nuxtjs/yandex-metrika', {
-    //   id: '25598768',
-    //   webvisor: true,
-    //   clickmap:true,
-    //   // useCDN:false,
-    //   trackLinks:true,
-    //   // accurateTrackBounce:true,
-    //   }
-    // ],
+    ['@nuxtjs/yandex-metrika', {
+      id: '25598768',
+      webvisor: true,
+      clickmap:true,
+      // useCDN:false,
+      trackLinks:true,
+      // accurateTrackBounce:true,
+      }
+    ],
     '@nuxtjs/feed',
     '@nuxtjs/axios',
     '@nuxtjs/auth',
