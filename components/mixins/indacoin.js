a4aa1f00913838ce9260aa6085fb29c3fce8fe1a
pashtet <pashtetbezd@gmail.com>
2018-12-24 10:17:46 +0700

    indacoin refactors

--- components/mixins/indacoin.js
+++ components/mixins/indacoin.js
@@ -1,23 +1,13 @@
 export const indacoinMixin = {
   methods: {
-
     coinBuyUrl: function(symbol) {
       return '/buy-coin?from=USD&to=' + symbol
     },
-
     isActiveCoin: function(symbol) {
-      let coin = this.getCoinBySymbol(symbol)
-      return coin !== null;
-    },
-
-    getCoinBySymbol: function(symbol) {
       let storeObj = this.$store.state.indacoin
-      let coin = null;
-      if( storeObj && storeObj.coins ) {
-        coin = storeObj.coins.find( el => el.attributes.symbol == symbol)
-      }
-      return coin;
+      return  storeObj 
+              && storeObj.coins 
+              && storeObj.coins.find( el => el.attributes.symbol == symbol)
     }
-
   }
 }
--- middleware/stats.js
+++ middleware/stats.js
@@ -1,37 +1,15 @@
 export default async function ({ app, store, redirect }) {
+  
   if( store.state.indacoin.updatedAt ) {
     return
   }
 
-  let indacoinCoins = null
-
   try {
-    indacoinCoins = await app.$axios.get(`/api/coin/indacoin-coins?per-page=500`)
+    let indacoinCoins = await app.$axios.get(`/api/coin/indacoin-coins?per-page=500`)
+    store.commit('SET_INDACOIN_COINS', indacoinCoins.data.data)
   } catch (e) {
-    indacoinCoins = null
     console.error('indacoin error')
   }
-
-  if( indacoinCoins == null) {
-    return
-  }
-
-  // const arrayToObject = (array, keyField) =>
-  //  array.reduce((obj, item) => {
-  //    obj[item[keyField]] = item
-  //    return obj
-  //  }, {})
-
-
-  //const ordered = arrayToObject(indacoinCoins.data.data, "id")
-
-  // Object.keys(indacoinCoins.data.data).sort().forEach(function(key, element) {
-
-
-  //   ordered[element.id] = element;
-  // });
-
-  store.commit('SET_INDACOIN_COINS', indacoinCoins.data.data)
 
 }
 
