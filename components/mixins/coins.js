379311915b740e1a51d0d6200e3faff693b796f2
pashtet <pashtetbezd@gmail.com>
2018-12-25 12:09:56 +0700

    formatPrice доделочка

--- components/mixins/coins.js
+++ components/mixins/coins.js
@@ -1,16 +1,17 @@
 export const coinsMixin = {
   methods: {
     formatPrice(value) {
-      if( value >= 0.5 && value < 100) {
+      let valueABS = Math.abs(value)
+      if( valueABS >= 0.5 && valueABS < 100) {
         return (value/1).toFixed(2)
       }
-      if( value >= 0.1 && value < 0.5) {
+      if( valueABS >= 0.1 && valueABS < 0.5) {
         return +(value/1).toFixed(3)
       }
-      if( value >= 0.01 && value < 0.1) {
+      if( valueABS >= 0.01 && valueABS < 0.1) {
         return +(value/1).toFixed(5)
       }
-      if( value < 0.01) {
+      if( valueABS < 0.01) {
         return +(value/1).toFixed(8)
       }
       let val = (value/1).toFixed(0)
